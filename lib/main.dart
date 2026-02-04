5e922d342e8509903e579b9c2205edccc0dc5ca1
MeePooh <poorinat2004@gmail.com>
2026-02-04 16:45:55 +0700

    fixed stat ui and add authen task 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'src/routing/app_router.dart';
-import 'src/common/app_theme.dart';
+import 'src/app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const ProviderScope(child: MyApp()));
-}
-
-class MyApp extends ConsumerWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final router = ref.watch(goRouterProvider);
-    final themeMode = ref.watch(themeModeProvider);
-    
-    return MaterialApp.router(
-      title: 'LifeGuardian',
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      themeMode: themeMode,
-      routerConfig: router,
-    );
-  }
+  runApp(const ProviderScope(child: LifeguardianApp()));
 }
